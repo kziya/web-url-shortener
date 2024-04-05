6e3b921ea245864401a3194abf679f5d1a81dc5f
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-05 22:54:58 +0300

    add: Not auth guard

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -3,13 +3,21 @@ import styles from './app.module.scss';
 import { AuthProvider } from './auth/AuthProvider';
 import { Route, Routes } from 'react-router-dom';
 import { AuthGuard } from './auth/AuthGuard';
+import { NotAuthGuard } from './auth/NotAuthGuard';
 
 export function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<AuthGuard>Hello</AuthGuard>} />
-        <Route path="/login" element={<h1>Login</h1>} />
+        <Route
+          path="/login"
+          element={
+            <NotAuthGuard>
+              <h1>Login</h1>
+            </NotAuthGuard>
+          }
+        />
       </Routes>
     </AuthProvider>
   );
---
+++ apps/client/src/app/auth/NotAuthGuard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useAuth } from './AuthContext';
+import { Navigate } from 'react-router-dom';
+
+export const NotAuthGuard: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { authData } = useAuth();
+
+  if (authData) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+};
