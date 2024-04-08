07a7d24aa96d154cd263019073c592e0edadb36f
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-08 22:18:45 +0300

    add: Not found page

--- apps/client/src/app/pages/NotFound/NotFound.tsx
+++ apps/client/src/app/pages/NotFound/NotFound.tsx
@@ -1,10 +1,27 @@
+import styles from './not-found.module.scss';
 import { Typography } from '@mui/material';
+import { Link } from '@mui/joy';
 
 export function NotFound() {
   return (
-    <main>
-      <Typography variant="h1" />
-      Not Found
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div>
+          <Typography variant="h1">404 Not Found</Typography>
+        </div>
+        <div className={styles.linkPlace}>
+          <Link
+            href="/"
+            variant="solid"
+            underline="none"
+            color="primary"
+            fontSize="24px"
+            borderRadius="5px"
+          >
+            Back to home
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
