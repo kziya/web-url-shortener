50fd6460e89ed3fa6214a9f2d1415cc24a89e23c
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-11 23:13:07 +0300

    fix: Mongodb connection uri

--- apps/server/src/app/config/module/mongoose-module.config.ts
+++ apps/server/src/app/config/module/mongoose-module.config.ts
@@ -4,13 +4,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 export const MongooseModuleConfig: MongooseModuleAsyncOptions = {
   imports: [ConfigModule],
   useFactory: (configService: ConfigService) => {
-    const user = configService.get('MONGODB_USER');
-    const password = configService.get('MONGODB_PASSWORD');
-    const host = configService.get('MONGODB_HOST');
-    const name = configService.get('MONGODB_NAME');
-
     return {
-      uri: `mongodb+srv://${user}:${password}@${host}/${name}?retryWrites=true&w=majority&appName=web-url-shortener`,
+      uri: configService.get('MONGODB_CONNECTION_URI'),
     };
   },
   inject: [ConfigService],
