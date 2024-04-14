c6541f89d457f98d6b71b91424f50c3ffff97461
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-14 10:03:35 +0300

    fix: Proxy host

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   const port = process.env.APP_PORT || 3000;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
