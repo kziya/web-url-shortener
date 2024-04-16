efd0677ac6f8ca67c254ace3e57cb03a001d2070
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-16 18:11:35 +0300

    fix: Using env variables

--- apps/client/src/app/config/AppConfiguration.ts
+++ apps/client/src/app/config/AppConfiguration.ts
@@ -1,4 +1,5 @@
 export default {
   API_URL:
-    process.env.REACT_APP_API_URL || 'https://api.url-shortener.kziya.com/api',
+    process.env.NX_REACT_APP_API_URL ||
+    'https://api.url-shortener.kziya.com/api',
 };
