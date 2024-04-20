96cc3981396756eb0eb30a2d8b938cec72f9fee1
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-20 21:28:48 +0300

    add: mailer module to server

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,18 +2,21 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { MailerModule } from '@nestjs-modules/mailer';
 
-import { ConfigModuleConfig } from './config/module/config-module.config';
-import { MongooseModuleConfig } from './config/module/mongoose-module.config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModuleConfig } from './config/module/config-module.config';
+import { MongooseModuleConfig } from './config/module/mongoose-module.config';
 import { RedisModuleConfig } from './config/module/redis-module.config';
+import { MailerModuleConfig } from './config/module/mailer-module.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(ConfigModuleConfig),
     MongooseModule.forRootAsync(MongooseModuleConfig),
     RedisModule.forRootAsync(RedisModuleConfig),
+    MailerModule.forRootAsync(MailerModuleConfig),
     AuthModule,
     UserModule,
   ],
