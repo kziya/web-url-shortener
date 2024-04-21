f0626bc1799ff8c557fd0dcfbba5022d51184bc9
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-21 21:31:37 +0300

    fix: Uid not valid exception message

--- apps/server/src/app/auth/exceptions/uid-not-valid-or-expired.exception.ts
+++ apps/server/src/app/auth/exceptions/uid-not-valid-or-expired.exception.ts
@@ -2,6 +2,6 @@ import { BadRequestException } from '@nestjs/common';
 
 export class UidNotValidOrExpiredException extends BadRequestException {
   constructor() {
-    super('VERIFY_UID_NOT_EXISTS_OR_EXPIRED');
+    super('UID_NOT_EXISTS_OR_EXPIRED');
   }
 }
