227329f3660c80c7ab25df60990a18b26aeefd9a
Maksym Chyrkov <chyrkovmaksym1@gmail.com>
2024-05-15 00:20:44 +0300

    update: Changed reset password styles

--- apps/client/src/app/pages/Auth/ResetPasswordByUid.tsx
+++ apps/client/src/app/pages/Auth/ResetPasswordByUid.tsx
@@ -1,9 +1,10 @@
-import { Alert, Button, TextField } from '@mui/material';
+import { Alert, Button, InputLabel, TextField, styled } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { Loading } from '../Loading/Loading';
 import AuthHttpService from '../../auth/services/AuthHttpService';
 import { NotFound } from '../NotFound/NotFound';
+import AuthLayout from './AuthLayout';
 
 export function ResetPasswordByUid() {
   const { uid } = useParams();
@@ -31,47 +32,46 @@ export function ResetPasswordByUid() {
   }
 
   return (
-    <main>
-      <div>
-        <div>
-          <h1>Reset your password</h1>
-        </div>
+    <AuthLayout formTitle="Reset your password">
+      <Wrapper>
         {resetPassword ? (
           <Alert severity="success">
             Successfully reset password, you will be redirected to the login
             page
           </Alert>
         ) : (
-          <div>
+          <Wrapper>
             {error && (
               <>
                 <Alert severity="error">{'Something went wrong'}</Alert>
                 <br />
               </>
             )}
-            <div>
-              <TextField
-                id="password-input"
-                label="Password"
-                variant="outlined"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <br />
+            <Fields>
+              <TextFieldWrapper>
+                <StyledLabel>Password</StyledLabel>
+                <AuthTextField
+                  id="password-input"
+                  variant="outlined"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </TextFieldWrapper>
+              <TextFieldWrapper>
+                <StyledLabel>Confirm Password</StyledLabel>
+                <AuthTextField
+                  id="confirm-password-input"
+                  variant="outlined"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </TextFieldWrapper>
+            </Fields>
 
-              <TextField
-                id="confirm-password-input"
-                label="Confirm Password"
-                variant="outlined"
-                type="password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </div>
-
             <div>
-              <Button
+              <SubmitButton
                 variant="contained"
                 onClick={() => {
                   AuthHttpService.resetPassword(uid, password, confirmPassword)
@@ -85,11 +85,54 @@ export function ResetPasswordByUid() {
                 }}
               >
                 Reset password
-              </Button>
+              </SubmitButton>
             </div>
-          </div>
+          </Wrapper>
         )}
-      </div>
-    </main>
+      </Wrapper>
+    </AuthLayout>
   );
 }
+
+const Wrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const Fields = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: '20px',
+});
+
+const StyledLabel = styled(InputLabel)({
+  color: '#fff',
+});
+
+const TextFieldWrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  rowGap: '4px',
+});
+
+const AuthTextField = styled(TextField)({
+  '& .MuiInputBase-root': {
+    height: '42px',
+  },
+
+  '& input': {
+    zIndex: 1,
+  },
+
+  '& fieldset': {
+    background: '#eee',
+    borderRadius: '5px',
+  },
+});
+
+const SubmitButton = styled(Button)({
+  width: '100%',
+  marginTop: '15px',
+  background: '#2B5BD7',
+  boxShadow: 'none',
+});
