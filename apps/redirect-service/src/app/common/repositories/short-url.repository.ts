1b59e920cff604132bf4f3245ce1428abdd1cef1
Ziya Karimli <ziyakerimli04@gmail.com>
2024-05-17 23:02:42 +0300

    refactoring: Fix imports

--- apps/redirect-service/src/app/common/repositories/short-url.repository.ts
+++ apps/redirect-service/src/app/common/repositories/short-url.repository.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import {
   ShortUrl,
   ShortUrlDocument,
   ShortUrlStatus,
 } from '@web-url-shortener/domain';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class ShortUrlRepository {
