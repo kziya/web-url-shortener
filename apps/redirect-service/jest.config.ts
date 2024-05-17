0e4e79c0f7c383a34e4e68de4b8c3a744ca8a50f
Ziya Karimli <ziyakerimli04@gmail.com>
2024-05-17 22:16:28 +0300

    refactoring: Rename redirecter-service to redirect-service

--- apps/redirect-service/jest.config.ts
+++ apps/redirect-service/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'redirecter-service',
+  displayName: 'redirect-service',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/redirecter-service',
+  coverageDirectory: '../../coverage/apps/redirect-service',
 };
