2c2063459dda72cedb6744dd7669b366ea6ff92b
Maksym Chyrkov <chyrkovmaksym1@gmail.com>
2024-06-05 01:40:02 +0300

    Added pagination for urls list table

--- apps/client/src/app/short-url/ShortUrlContext.ts
+++ apps/client/src/app/short-url/ShortUrlContext.ts
@@ -4,12 +4,13 @@ import { createContext, useContext } from 'react';
 export interface IShortUrlContext {
   urlsList: FullShortUrl[];
   urlListLoading: boolean;
-  getUrlsList: (page: number, status?: ShortUrlStatus) => Promise<void>;
+  getUrlsList: (idLast: string, status?: ShortUrlStatus) => Promise<void>;
   createPrivateUrl: (url: string) => Promise<void>;
   createPublicUrl: (url: string) => Promise<void>;
   newPrivateUrlLoading: boolean;
   deleteUrl: (id: string) => Promise<void>;
   renewPrivateUrl: (id: string) => Promise<void>;
+  hasMoreUrls: boolean;
 }
 
 export const ShortUrlContext = createContext<IShortUrlContext>({
@@ -21,6 +22,7 @@ export const ShortUrlContext = createContext<IShortUrlContext>({
   newPrivateUrlLoading: false,
   deleteUrl: () => null,
   renewPrivateUrl: () => null,
+  hasMoreUrls: true,
 });
 
 export const useShortUrl = () => {
